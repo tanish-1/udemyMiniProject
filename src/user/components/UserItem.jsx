1f1e43bf89e127f7f97af9a8930c1ae931c7e735
tanish-1 <imtanishlamba.com>
2025-07-20 14:18:06 +0530

    adding userlist

---
+++ src/user/components/UserItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './UserItem.css'
+const UserItem = ({}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserItem;
---
+++ src/user/components/UsersList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './UsersList.css'
+import UserItem from "./UserItem.jsx";
+const UsersList = ({items}) => {
+   if(items.length === 0){
+       return <div className="center">
+           <h2>No users found. </h2>
+       </div>;
+   }
+   return <ul>
+       {items.map(user => (
+          <UserItem
+              key = {user.id}
+              id = {user.id}
+              image={ user.image }
+              name={user.name}
+              placeCount={user.places}
+          />
+       ))}
+   </ul>
+};
+
+export default UsersList;
