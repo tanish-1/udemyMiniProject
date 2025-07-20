90f9f56bce89470730df17b2b0c6313f8fc64dca
tanish-1 <imtanishlamba.com>
2025-07-20 12:29:30 +0530

    routes setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Routes, Route, Navigate} from 'react-router-dom';
+import User from './user/pages/User'
+import NewPlace from "./places/pages/NewPlace.jsx";
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <Routes>
+         <Route path="/" element={<User />} />
+            <Route path="/places/new" element={<NewPlace />} />
+            {/* Catch-all route for any unmatched paths */}
+            <Route path="*" element={<Navigate to="/" replace/>} />
+        </Routes>
     );
 };
 
---
+++ src/user/pages/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            User Works
+
+        </div>
+    );
+};
+
+export default User;
