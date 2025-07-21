709607e04862ac3f0cecf5398f6af85419fdcd88
tanish-1 <imtanishlamba.com>
2025-07-21 14:02:44 +0530

    adding main header

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { Routes, Route, Navigate} from 'react-router-dom';
 import User from './user/pages/Users.jsx'
 import NewPlace from "./places/pages/NewPlace.jsx";
+import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
+
 const App = () => {
     return (
+        <>
+        <MainNavigation />
+            <main>
         <Routes>
          <Route path="/" element={<User />} />
             <Route path="/places/new" element={<NewPlace />} />
             {/* Catch-all route for any unmatched paths */}
             <Route path="*" element={<Navigate to="/" replace/>} />
         </Routes>
+            </main>
+        </>
     );
 };
 
--- src/user/components/UsersList.jsx
+++ src/user/components/UsersList.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import UserItem from './UserItem';
 import './UsersList.css';
-
+import Card from "../../shared/components/UIElements/Card.jsx";
 const UsersList = (props) => {
     if (props.items.length === 0) {
         return (
             <div className="center">
+                <Card>
                 <h2>No users found.</h2>
+                </Card>
             </div>
         );
     }
