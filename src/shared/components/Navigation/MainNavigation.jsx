631c34d7479a3387e720e17b45f56aa34111031e
tanish-1 <imtanishlamba.com>
2025-07-21 16:52:41 +0530

    navlink responsive done

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './MainNavigation.css'
 import {Link} from "react-router-dom";
 import MainHeader from "./MainHeader";
+import SideDrawer from "./SideDrawer.jsx";
 import NavLinks from "./NavLinks.jsx";
 const MainNavigation = (props) => {
     return (
+        <>
+        <SideDrawer>
+            <nav className="main-navigation__drawer-nav">
+                <NavLinks />
+            </nav>
+        </SideDrawer>
       <MainHeader>
           <button className="main-navigation__menu-btn">
               <span></span>
@@ -14,10 +21,11 @@ const MainNavigation = (props) => {
           <h1 className="main-navigation__title">
              <Link to = "/">YourPlaces</Link>
           </h1>
-          <nav>
+          <nav className="main-navigation__header-nav">
             <NavLinks/>
           </nav>
       </MainHeader>
+        </>
     );
 };
 
