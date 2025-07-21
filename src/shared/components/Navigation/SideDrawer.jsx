5180a296e816564f60dafe0987370b69b547fb31
tanish-1 <imtanishlamba.com>
2025-07-21 21:36:49 +0530

    portals added

--- src/shared/components/Navigation/SideDrawer.jsx
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './SideDrawer.css'
+import ReactDom from 'react-dom';
 const SideDrawer = (props) => {
-    return (
-        <aside className="side-drawer ">{props.children}</aside>
-    );
+       const content = <aside className="side-drawer ">{props.children}</aside>;
+       return ReactDom.createPortal(content, document.getElementById('drawer-hook'))
+
 };
 
 export default SideDrawer;
