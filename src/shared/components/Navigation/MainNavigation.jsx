179f512f748ce3351982347b029005349f234920
tanish-1 <imtanishlamba.com>
2025-07-21 16:16:30 +0530

    navlinks added

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './MainNavigation.css'
 import {Link} from "react-router-dom";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks.jsx";
 const MainNavigation = (props) => {
     return (
       <MainHeader>
@@ -14,7 +15,7 @@ const MainNavigation = (props) => {
              <Link to = "/">YourPlaces</Link>
           </h1>
           <nav>
-              ...
+            <NavLinks/>
           </nav>
       </MainHeader>
     );
