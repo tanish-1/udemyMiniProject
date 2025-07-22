c0c65086abcc4106157e71dd23ce8ce9e3a47d1c
tanish-1 <imtanishlamba.com>
2025-07-22 16:54:43 +0530

    dynamicRrouteSegement

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate} from 'react-router-dom';
 import User from './user/pages/Users.jsx'
 import NewPlace from "./places/pages/NewPlace.jsx";
 import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
-
+import UserPlace from "./places/pages/UserPlace.jsx";
 const App = () => {
     return (
         <>
@@ -11,6 +11,7 @@ const App = () => {
             <main>
         <Routes>
          <Route path="/" element={<User />} />
+            <Route path="/:userId/places" element={<UserPlace />}/>
             <Route path="/places/new" element={<NewPlace />} />
             {/* Catch-all route for any unmatched paths */}
             <Route path="*" element={<Navigate to="/" replace/>} />
