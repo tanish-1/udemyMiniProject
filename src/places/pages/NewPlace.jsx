0ebb1660f90af0ef81fd7a5c65a599ba0997ba0c
tanish-1 <imtanishlamba.com>
2025-08-04 14:30:05 +0530

    sharing inputValues&AddingMultipleInputs

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import './NewPlace.css'
 import Input from "../../shared/components/FormElements/Input.jsx";
-import {VALIDATOR_REQUIRE} from "../../shared/util/validators.jsx";
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/util/validators.jsx";
 
 const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {
+
+    },[])
+    const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+    },[])
     return (
       <form className="place-form">
        <Input
+           id="title"
            element="input"
            type="text"
            label="Title"
            validators={[VALIDATOR_REQUIRE()]}
-           errorText="Please enter a valid title." />
+           errorText="Please enter a valid title."
+           onInput={titleInputHandler}
+       />
+          <Input
+              id="description"
+              element="textarea"
+              label="Description"
+              validators={[VALIDATOR_MINLENGTH(5)]}
+              errorText="Please enter a valid description (at least 5 characters)."
+              onInput={descriptionInputHandler}
+          />
       </form>
     );
 };
--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import './Input.css'
 
 import {validate} from "../../util/validators.jsx";
@@ -27,6 +27,11 @@ const Input = (props) => {
        isValid: false
        });
 
+   const { id , onInput } = props
+    const { value , isValid} = inputState;
+   useEffect(() => {
+      onInput(id, value, isValid)
+   },[id, value, isValid, onInput])
 
     const changeHandler = event => {
        dispatch({
