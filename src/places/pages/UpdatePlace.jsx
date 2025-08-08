69fcd8edecc03a7ded3c70fa9136ae31d80dfa61
tanish-1 <imtanishlamba.com>
2025-08-08 19:48:31 +0530

    fixing minor issues

--- src/places/pages/UpdatePlace.jsx
+++ src/places/pages/UpdatePlace.jsx
@@ -9,6 +9,7 @@ import {
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import './PlaceForm.css';
+import Card from "../../shared/components/UIElements/Card.jsx";
 
 const DUMMY_PLACES = [
     {
@@ -26,7 +27,7 @@ const DUMMY_PLACES = [
     },
     {
         id: 'p2',
-        title: 'Empire State Building',
+        title: 'Emp. State Building',
         description: 'One of the most famous sky scrapers in the world!',
         imageUrl:
             'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
@@ -60,19 +61,22 @@ const UpdatePlace = () => {
     const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
     useEffect(() => {
-        setFormData(
-            {
-                title: {
-                    value: identifiedPlace.title,
-                    isValid: true
+        if(identifiedPlace){
+            setFormData(
+                {
+                    title: {
+                        value: identifiedPlace.title,
+                        isValid: true
+                    },
+                    description: {
+                        value: identifiedPlace.description,
+                        isValid: true
+                    }
                 },
-                description: {
-                    value: identifiedPlace.description,
-                    isValid: true
-                }
-            },
-            true
-        );
+                true
+            );
+        }
+
         setIsLoading(false);
     }, [setFormData, identifiedPlace]);
 
@@ -84,7 +88,9 @@ const UpdatePlace = () => {
     if (!identifiedPlace) {
         return (
             <div className="center">
+                <Card>
                 <h2>Could not find place!</h2>
+                </Card>
             </div>
         );
     }
