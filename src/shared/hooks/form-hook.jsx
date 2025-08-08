ee1afc7ac51b4fe53cc332a1bd236e927c3ddfa5
tanish-1 <imtanishlamba.com>
2025-08-08 19:15:42 +0530

    adjist the form hook

--- src/shared/hooks/form-hook.jsx
+++ src/shared/hooks/form-hook.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useReducer } from 'react';
+import { useCallback, useReducer } from 'react';
+
 const formReducer = (state, action) => {
     switch (action.type) {
         case 'INPUT_CHANGE':
@@ -18,15 +19,22 @@ const formReducer = (state, action) => {
                 },
                 isValid: formIsValid
             };
+        case 'SET_DATA':
+            return {
+                inputs: action.inputs,
+                isValid: action.formIsValid
+            };
         default:
             return state;
     }
 };
-export const useForm = (initialInputs, initialFormValidity) =>{
+
+export const useForm = (initialInputs, initialFormValidity) => {
     const [formState, dispatch] = useReducer(formReducer, {
         inputs: initialInputs,
         isValid: initialFormValidity
     });
+
     const inputHandler = useCallback((id, value, isValid) => {
         dispatch({
             type: 'INPUT_CHANGE',
@@ -36,5 +44,13 @@ export const useForm = (initialInputs, initialFormValidity) =>{
         });
     }, []);
 
-    return [formState, inputHandler];
-}
+    const setFormData = useCallback((inputData, formValidity) => {
+        dispatch({
+            type: 'SET_DATA',
+            inputs: inputData,
+            formIsValid: formValidity
+        });
+    }, []);
+
+    return [formState, inputHandler, setFormData];
+};
