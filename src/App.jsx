0de05e109a1e94f41f7e5eb177c23d1ce13c9966
tanish-1 <imtanishlamba.com>
2025-08-08 12:21:11 +0530

    start with update place

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import User from './user/pages/Users.jsx'
 import NewPlace from "./places/pages/NewPlace.jsx";
 import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
 import UserPlace from "./places/pages/UserPlace.jsx";
+import UpdatePlace from "./places/pages/UpdatePlace.jsx";
 const App = () => {
     return (
         <>
@@ -13,6 +14,7 @@ const App = () => {
          <Route path="/" element={<User />} />
             <Route path="/:userId/places" element={<UserPlace />}/>
             <Route path="/places/new" element={<NewPlace />} />
+            <Route path="/places/:placeId" element={<UpdatePlace/>} />
             {/* Catch-all route for any unmatched paths */}
             <Route path="*" element={<Navigate to="/" replace/>} />
         </Routes>
